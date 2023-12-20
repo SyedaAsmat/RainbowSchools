3705e8eed9e9ff66ea007aa4c8b18e2dd5f5273d
HP <HP@DESKTOP-AQ07QGH>
2023-12-20 20:48:53 +0530

    Added Successfully

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace RainbowSchools
+{
+    namespace RainbowSchools
+    {
+        internal class Program
+        {
+            public static void FileFun()
+            {
+                string path = @"C:\Users\HP\source\repos\RainbowSchools\RainbowSchools\StudentData.txt";
+
+                if (File.Exists(path))
+                {
+                    StreamReader sr = File.OpenText(path);
+                    string tillEnd = sr.ReadToEnd();
+                    Console.WriteLine(tillEnd);
+                    sr.Close();
+                }
+                else
+                {
+                    Console.WriteLine("File Not Found\nPlease Create StudentData.txt file\n");
+                }
+            }
+            static void Main(string[] args)
+            {
+                FileFun();
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+Steps to execute the code:
+
+1. Click on Code button then Download zip
+2. Extract the files from zip folder
+3. Open the code using Visual Studio
+4. Run/Execute the code to see output
